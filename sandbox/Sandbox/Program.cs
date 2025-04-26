f42b8241a50bcd8447c209c5a9e785a8ca4c3abc
kayleegood22 <kayleegood22@byui.edu>
2025-04-26 13:42:33 -0600

    prep1 and prep2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // This is a program to compute the area of a circle.
+        // Get the radius of the circle from the user.
+        Console.Write("Please enter the radius: ");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        // Compute the area of the circle.
+        double area = Math.PI * radius * radius;
+
+        /* Display the area for the user to see.
+        */
+        Console.WriteLine($"Area of the circle: {area}");
     }
 }
