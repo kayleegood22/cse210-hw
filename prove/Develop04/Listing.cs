7ad426a5f6cd4c36fcdb4a8eb66fb298caef6419
kayleegood22 <kayleegood22@byui.edu>
2025-06-14 22:48:55 -0600

    additional features

--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -1,63 +1,64 @@
-
-
 using System;
 using System.Collections.Generic;
 using System.Threading;
 
 public class ListingActivity : Activity
 {
-    private List<string> _prompts;
+    private List<string> _usedPrompts = new List<string>();
 
-    public ListingActivity() : base("Listing", 
-        "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+    private static List<string> _prompts = new List<string>
     {
-        _prompts = new List<string>
-        {
-            "Who are people that you appreciate?",
-            "What are personal strengths of yours?",
-            "Who are people that have helped you this week?",
-            "When have you felt the Holy Ghost this month?",
-            "Who are some of your personal heroes?"
-        };
-    }
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    public ListingActivity() : base(
+        "Listing",
+        "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+    { }
 
     protected override void PerformActivity()
-{
-    string prompt = GetRandomPrompt();
-    Console.WriteLine($"\nPrompt:\n--- {prompt} ---");
-    Console.WriteLine("You will begin in:");
-    ShowCountdown(3);
-
-    Console.WriteLine("\nStart listing items. Press Enter after each item:");
-
-    List<string> userItems = new List<string>();
-    DateTime endTime = DateTime.Now.AddSeconds(GetDuration());
-
-    while (DateTime.Now < endTime)
     {
-        Console.Write("> ");
-        string input = Console.ReadLine();
+        string prompt = GetNextPrompt();
+        Console.WriteLine("\nList as many responses as you can to the following prompt:");
+        Console.WriteLine($"--- {prompt} ---");
+        Console.Write("You may begin in: ");
+        ShowCountdown(5);
 
-        // Stop if input takes too long (time exceeded)
-        if (DateTime.Now >= endTime)
+        DateTime endTime = DateTime.Now.AddSeconds(GetDuration());
+        int itemCount = 0;
+
+        Console.WriteLine("\nStart listing items. Press Enter after each:");
+
+        while (DateTime.Now < endTime)
         {
-            break;
+            // Check if there's still time left before prompting
+            if (Console.KeyAvailable)
+            {
+                Console.ReadLine();
+                itemCount++;
+            }
         }
 
-        if (!string.IsNullOrWhiteSpace(input))
-        {
-            userItems.Add(input);
-        }
+        Console.WriteLine($"\nYou listed {itemCount} items!");
     }
 
-    Console.WriteLine($"\nYou listed {userItems.Count} items.");
-}
-
-
-    private string GetRandomPrompt()
+    private string GetNextPrompt()
     {
-        Random random = new Random();
-        int index = random.Next(_prompts.Count);
-        return _prompts[index];
+        if (_usedPrompts.Count == _prompts.Count)
+            _usedPrompts.Clear();
+
+        string prompt;
+        do
+        {
+            prompt = _prompts[new Random().Next(_prompts.Count)];
+        } while (_usedPrompts.Contains(prompt));
+
+        _usedPrompts.Add(prompt);
+        return prompt;
     }
 }
+
--- prove/Develop04/Reflection.cs
+++ prove/Develop04/Reflection.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 public class ReflectionActivity : Activity
 {
-    private List<string> _prompts = new List<string>
+    private List<string> _usedPrompts = new List<string>();
+    private List<string> _usedQuestions = new List<string>();
+    
+    private static List<string> _prompts = new List<string>
     {
         "Think of a time when you stood up for someone else.",
         "Think of a time when you did something really difficult.",
@@ -11,7 +15,7 @@ public class ReflectionActivity : Activity
         "Think of a time when you did something truly selfless."
     };
 
-    private List<string> _questions = new List<string>
+    private static List<string> _questions = new List<string>
     {
         "Why was this experience meaningful to you?",
         "Have you ever done anything like this before?",
@@ -24,22 +28,62 @@ public class ReflectionActivity : Activity
         "How can you keep this experience in mind in the future?"
     };
 
-    public ReflectionActivity() : base("Reflection", "This activity will help you reflect on moments when you showed strength and resilience.") {}
+    public ReflectionActivity() : base(
+        "Reflection",
+        "This activity will help you reflect on times in your life when you have shown strength and resilience. " +
+        "This will help you recognize the power you have and how you can use it in other aspects of your life.")
+    { }
 
     protected override void PerformActivity()
     {
-        Random rand = new Random();
-        string prompt = _prompts[rand.Next(_prompts.Count)];
-        Console.WriteLine("\nConsider the following prompt:");
-        Console.WriteLine($"> {prompt}");
-        Console.WriteLine("\nNow reflect on the following questions:");
+        string prompt = GetNextPrompt();
+        Console.WriteLine($"\nConsider the following prompt:\n--- {prompt} ---");
+        Console.WriteLine("\nWhen you have something in mind, press Enter to continue.");
+        Console.ReadLine();
+
+        Console.WriteLine("Now ponder each of the following questions as they relate to this experience.");
+        Console.Write("You may begin in: ");
+        ShowCountdown(3);
+        Console.Clear();
 
         DateTime endTime = DateTime.Now.AddSeconds(GetDuration());
+
         while (DateTime.Now < endTime)
         {
-            string question = _questions[rand.Next(_questions.Count)];
-            Console.WriteLine($"> {question}");
+            string question = GetNextQuestion();
+            Console.Write($"> {question} ");
             ShowSpinner(5);
+            Console.WriteLine();
         }
+    }
+
+    private string GetNextPrompt()
+    {
+        if (_usedPrompts.Count == _prompts.Count)
+            _usedPrompts.Clear();
+
+        string prompt;
+        do
+        {
+            prompt = _prompts[new Random().Next(_prompts.Count)];
+        } while (_usedPrompts.Contains(prompt));
+
+        _usedPrompts.Add(prompt);
+        return prompt;
+    }
+
+    private string GetNextQuestion()
+    {
+        if (_usedQuestions.Count == _questions.Count)
+            _usedQuestions.Clear();
+
+        string question;
+        do
+        {
+            question = _questions[new Random().Next(_questions.Count)];
+        } while (_usedQuestions.Contains(question));
+
+        _usedQuestions.Add(question);
+        return question;
     }
 }
